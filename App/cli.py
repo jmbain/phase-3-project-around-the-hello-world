d25067e22fd8b649471276df39beb2f9f7a1b1d2
Josh Bain <joshuambain@gmail.com>
2024-09-06 15:40:55 -0400

    created sample starting code in cli.py and initiated some db files

---
+++ App/cli.py
@@ -0,0 +1,35 @@
+import random
+import time
+
+def run():
+    """This function runs the main game menu, which includes 
+    1. Play Game
+    2. Leaderboard
+    3. Advanced Stats (optional)
+    4. Quit
+    """
+    pass
+
+def play():
+    """Runs when user selects Play Game from main game menu..."""
+    pass
+
+def attempt_and_score():
+    """Runs when a user attempts a question, function also keeps track of number of attempts and score if correct/incorrect"""
+
+
+def show_leaderboard():
+    """Runs when user selects Leaderboard from main game menu..."""
+    pass
+
+def show_advanced_stats():
+    """Runs when user selects advanced stats from main game menu..."""
+    pass
+
+def quit():
+    """Runs when user selects quit from main game menu..."""
+    pass
+
+
+if __name__ == '__main__':
+    run()
